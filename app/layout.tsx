61053992a366f9cffe84b738d4d051c564b0e72b
Kenny Pflug <kenny.pflug@live.de>
2023-06-18 15:49:07 +0200

    enabled eslint semicolon (semi) rule

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
     title: 'feO2x.com',
-    description: "Kenny Pflug's Website and Blog ",
-}
+    description: "Kenny Pflug's Website and Blog"
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode}) {
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
         </html>
-    )
+    );
 }
